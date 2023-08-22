d7645791270b855570e38ea2803701e51450346a
G3n3sis-x <parisileonardo15@gmail.com>
2023-08-22 22:51:43 +0200

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,50 +1,32 @@
 import React from "react";
-import styled from "@emotion/styled";
-import { Tooltip } from "@mui/material";
-import { BiLeaf } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
-function Card(props) {
-  const { header } = props;
-  const truncatedHeader =
-    header.length > 22 ? header.slice(0, 22) + "..." : header;
-
+function Card({ recipe }) {
   return (
-    <div className="relative flex flex-col justify-center w-[18rem] h-[260px] box-border p-4 rounded-md bg-transparent transition-shadow duration-1000 ease-out hover:shadow-3xl hover:duration-100">
-      <ImgContainer
-        image={props.image}
-        className="w-full m-auto h-full min-h-[70%] bg-cover object-cover rounded-md"
-      ></ImgContainer>
-      <div className="flex w-full justify-center my-4">
-        <h1 className="text-xl font-satoshi font-medium text-[#303030] w-full">
-          {truncatedHeader}
-        </h1>
-        <h2 className="font-satoshi font-normal p-1 text-[#303030] text-sm">
-          {props.minutes ? props.minutes + "`" : ""}
-        </h2>
+    <div className="flex flex-col gap-3">
+      <div className="object-cover w-full">
+        <img src={recipe.image} alt="image" className="w-full rounded-[30px]" />
       </div>
-      {props.veggie ? (
-        <Tooltip title="vegetarian" placement="right">
-          <div className="absolute -top-2 -right-2 p-1 bg-emerald-600 rounded-lg text-md cursor-pointer">
-            <BiLeaf size={14} />
+      <div className="bg-[#fbfafa] hover:bg-[#ededed] font-satoshi transition rounded-[30px] flex flex-col p-5 justify-between w-full gap-4">
+        <h1 className="text-[#222528] text-lg font-semibold">{recipe.title}</h1>
+        <p
+          dangerouslySetInnerHTML={{ __html: recipe.summary }}
+          className="text-[#616161] text-base font-satoshi font-normal line-clamp-3"
+        ></p>
+        <div className="flex justify-between items-center">
+          <Link
+            to={`/recipe/${recipe.id}`}
+            className="p-2 bg-[#098149] font-satoshi text-white rounded-md"
+          >
+            View more
+          </Link>
+          <div className="font-satoshi text-base font-medium">
+            servings: {recipe.servings}
           </div>
-        </Tooltip>
-      ) : null}
+        </div>
+      </div>
     </div>
   );
 }
-
-//styling
-const VeggieCardContainer = styled.div`
-  background: url(${(props) => props.image});
-  object-fit: cover;
-  @media screen and (max-width: 768px) {
-    width: 340px;
-  }
-  position: relative;
-`;
-
-const ImgContainer = styled.div`
-  background: url(${(props) => props.image});
-`;
 
 export default Card;
